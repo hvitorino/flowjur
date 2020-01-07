acf21c3af72fda6ada5c65fca02c8b7244d6e9a3
Hamon Vitorino <hamon.vitorino@invillia.com>
2020-01-07 14:09:25 -0300

    project infos fixed

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject flowjur "0.1.0"
-  :description "flowjur is a Clojure library designed to run stateful functions as part of a workflow."
-  :url "http://example.com/FIXME"
+  :description "Flowjur is a Clojure library designed to run stateful functions as part of a workflow."
+  :url "https://github.com/hvitorino/flowjur"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url  "https://github.com/hvitorino/flowjur"}
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :repl-options {:init-ns flowjur.core}
   :profiles {:kaocha {:dependencies [[lambdaisland/kaocha "0.0-565"]]}}
