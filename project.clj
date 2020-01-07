545d5e0a2122d33c584e4e908f3e9ad0e3186665
Hamon Vitorino <hamon.vitorino@invillia.com>
2020-01-07 14:03:40 -0300

    url add to project file

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url  "https://www.eclipse.org/legal/epl-2.0/"}
+            :url  "https://github.com/hvitorino/flowjur"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :repl-options {:init-ns flowjur.core}
   :profiles {:kaocha {:dependencies [[lambdaisland/kaocha "0.0-565"]]}}
